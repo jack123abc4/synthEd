69aeb9a70477fc7e6ca4252962506b89be6500e2
Lauren <lburke522@gmail.com>
2022-10-24 15:43:00 -0400

    got navbar started

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
 
 export default function Nav() {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
   return (
     <div className='header'>
         <Link to='/'>
             <h1>SynthEd.</h1>
         </Link>
-        <ul>
+        <ul className={ click ? 'menu.active' : 'menu'}>
             <li>
                <Link to='/'>Home</Link> 
             </li>
-            <li>
+            {/* <li>
                <Link to='/pianoRoll'>Piano Roll</Link> 
-            </li>
+            </li> */}
             <li>
                <Link to='/piano'>Piano</Link> 
             </li>
@@ -27,8 +32,12 @@ export default function Nav() {
             <li>
                <Link to='/about'>About Us</Link> 
             </li>
-
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{color: 'white'}} />) : <FaBars size={20} style={{color: 'white'}} /> }
+            
+            
+        </div>
     </div>
   )
 }
