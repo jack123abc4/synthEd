f5f83353bd97d6d5ed74fb4827ba45176f521338
Lauren <lburke522@gmail.com>
2022-10-24 13:25:29 -0400

    conflict

---
+++ synthed/src/routes/Piano.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Piano = () => {
+  return (
+    <div>
+      Piano
+    </div>
+  )
+}
+
+export default Piano
---
+++ synthed/src/routes/PianoRoll.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PianoRoll = () => {
+  return (
+    <div>
+      Piano Roll
+    </div>
+  )
+}
+
+export default PianoRoll
