a3f3320fde1888534ddf89719829a4b8b7eafb0b
Lauren <lburke522@gmail.com>
2022-10-25 17:02:51 -0400

    about page donezo

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -4,26 +4,48 @@ import "./contact.scss";
 import emailjs from 'emailjs-com';
 
 export default function Contact() {
+  const formRef = useRef();
+  const [done, setDone] =useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_oel3aem', 'template_rz0wwlw', formRef.current, 'uOEZu1fDeZRQbIEq6')
+    .then((result) => {
+        console.log(result.text);
+        e.target.reset()
+        setDone(true)
+    }, (error) => {
+        console.log(error.text);
+    });
+  }
   return (
     <div className="contact">
       <div className="line"></div>
       <div className="contact-wrapper">
       <div className="left">
-        <h1 className="contact-intro">Share your musical journey</h1>
-        <div className="emailContainer">
-        <img src="assets/email.png" alt="" />
-      </div>
+
+        <div class="party">
+    <div class="body">
+    <div class="container">
+        <div class="ring"></div>
+        <div class="ring"></div>
+        <div class="ring"></div>
+    </div>
+    </div>
+    </div>
       </div>
       <div className="right">
         <p className="contact-info">
           <b>What's your tune?</b> Contact us below with any questions, comments, or just to say, "What's up?"!
         </p>
-        <form>
+        <form ref={formRef} onSubmit={handleSubmit}>
           <input type="text" placeholder="Name..." name="name" />
           <input type="text" placeholder="Email..." name="email" />
           <input type="text" placeholder="Subject..." name="subject" />
           <textarea rows="10" placeholder="Type your message here..." name="message" />
           <button type="submit">Submit</button>
+          <div className="thanks">
+          {done && "Thank you...We'll be in touch!"}
+          </div>
         </form>
       </div>
       </div>
