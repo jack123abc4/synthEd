db8213ee617b923eebd702f14a84c751ca87fa8a
Lauren <lburke522@gmail.com>
2022-10-25 10:03:39 -0400

    homepage started

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,1 +1,29 @@
+import './hero.css';
+import React from 'react';
 
+
+
+const Hero = () => {
+  return (
+    <div className="hero">
+
+<div class="muzieknootjes">
+  <div class="noot-1">
+  &#9835; &#9833;
+</div>
+<div class="noot-2">
+  &#9833;
+</div>
+<div class="noot-3">
+  &#9839; &#9834;
+</div>
+<div class="noot-4">
+  &#9834;
+</div>
+</div>
+
+</div>
+  )
+}
+
+export default Hero
