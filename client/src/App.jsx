49e5b98546be859d3a420e539c2204af85708b62
Lauren <lburke522@gmail.com>
2022-10-25 10:50:00 -0400

    implemented apollo and home page react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,28 @@ import Account from './routes/Account';
 import Piano from './routes/Piano';
 import PianoRoll from './routes/PianoRoll';
 import Resources from './routes/Resources';
-import { Routes, Route } from 'react-router-dom'
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <>
+    
+    <ApolloProvider  client ={client}>
+    <Router>
+      <>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/pianoRoll' element={<PianoRoll />} />
@@ -20,6 +37,8 @@ function App() {
       <Route path='/about' element={<About />} />
     </Routes>  
     </>
+    </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,13 +1,20 @@
 import './hero.css';
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import { init } from 'ityped';
+import { Link } from 'react-router-dom';
 
 
 const Hero = () => {
+    const textRef = useRef();
+    useEffect(() => {
+      init(textRef.current, { 
+        showCursor: true, 
+        backDelay: 1500,
+        strings: ["Learn", "Create", "Play", "Share"] })
+    }, []) 
   return (
     <div className="hero">
-
-<div class="muzieknootjes">
+        <div class="muzieknootjes">
   <div class="noot-1">
   &#9835; &#9833;
 </div>
@@ -21,6 +28,23 @@ const Hero = () => {
   &#9834;
 </div>
 </div>
+        <div className="mask">
+            <img className='hero-img' src='assets/thumb.png' />
+            
+        </div>
+        <div className="content">
+            
+            <h1>SynthEd.</h1>
+            <h3>
+                Music to <span ref={textRef}></span>
+            </h3>
+        
+        <div>
+            <Link to="/account" className='btn'>Sign Up</Link>
+            <Link to="/account" className='btn btn-light'>Login</Link>
+        </div>
+        </div>
+
 
 </div>
   )
