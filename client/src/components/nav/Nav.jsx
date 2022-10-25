1ffbc888de56c7a34bf2209e5de515ba58e7bcb4
Lauren <lburke522@gmail.com>
2022-10-25 09:17:10 -0400

    navbar progress, added mobile hamburger menu

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 import './nav.css';
 import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
-import { FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-export default function Nav() {
 
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
+const Nav = () => {
+   const [click, setClick] = useState(false);
+   const handleClick = () => setClick(!click);
   return (
-    <div className='header'>
+    <div className='nav'>
         <Link to='/'>
             <h1>SynthEd.</h1>
         </Link>
-        <ul className={ click ? 'menu.active' : 'menu'}>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                <Link to='/'>Home</Link> 
             </li>
@@ -21,7 +20,7 @@ export default function Nav() {
                <Link to='/pianoRoll'>Piano Roll</Link> 
             </li> */}
             <li>
-               <Link to='/piano'>Piano</Link> 
+               <Link to='/piano'>Piano Player</Link> 
             </li>
             <li>
                <Link to='/resources'>Resources</Link> 
@@ -33,11 +32,13 @@ export default function Nav() {
                <Link to='/about'>About Us</Link> 
             </li>
         </ul>
-        <div className="hamburger" onClick={handleClick}>
-            {click ? (<FaTimes size={20} style={{color: 'white'}} />) : <FaBars size={20} style={{color: 'white'}} /> }
+         <div className='hamburger' onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{color: 'white'}} />) : (<FaBars size={20} style={{color: 'white'}} />)}
             
             
-        </div>
+         </div>
     </div>
   )
 }
+
+export default Nav
