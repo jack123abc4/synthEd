f22e738a9435ce1fb26142b356d2afdb62982828
Lauren <lburke522@gmail.com>
2022-10-26 07:47:49 -0400

    hero formatting

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,53 +1,60 @@
-import './hero.css';
-import React, { useEffect, useRef } from 'react';
-import { init } from 'ityped';
-import { Link } from 'react-router-dom';
-
+import "./hero.css";
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
+import { Link } from "react-router-dom";
+import { BsMusicNote, BsMusicNoteBeamed } from "react-icons/bs";
+import { HiHashtag } from "react-icons/hi";
+import { SlMusicTone } from "react-icons/sl";
+import { GiMusicalNotes } from "react-icons/gi";
 
 const Hero = () => {
-    const textRef = useRef();
-    useEffect(() => {
-      init(textRef.current, { 
-        showCursor: true, 
-        backDelay: 1500,
-        strings: ["Learn", "Create", "Play", "Share"] })
-    }, []) 
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: ["Learn", "Create", "Play", "Share"],
+    });
+  }, []);
   return (
     <div className="hero">
-        <div class="muzieknootjes">
-  <div class="noot-1">
-  &#9835; &#9833;
-</div>
-<div class="noot-2">
-  &#9833;
-</div>
-<div class="noot-3">
-  &#9839; &#9834;
-</div>
-<div class="noot-4">
-  &#9834;
-</div>
-</div>
-        <div className="mask">
-            <img className='hero-img' src='assets/thumb.png' />
-            
+      <div class="music-notes">
+        <div class="note-1">
+          <BsMusicNoteBeamed /> <BsMusicNote />
         </div>
+        <div class="note-2">
+          <BsMusicNote />
+        </div>
+        <div class="note-3">
+          <HiHashtag /> <SlMusicTone />
+        </div>
+        <div class="note-4">
+          <GiMusicalNotes />
+        </div>
+      </div>
+      <div className="mask">
+        <img className="hero-img" src="assets/thumb.png" />
+        <div className="left"></div>
+      </div>
+      <div className="right">
         <div className="content">
-            
-            <h1>SynthEd.</h1>
-            <h3>
-                Music to <span ref={textRef}></span>
-            </h3>
-        
-        <div>
-            <Link to="/account" className='btn'>Sign Up</Link>
-            <Link to="/account" className='btn btn-light'>Login</Link>
+          <h1>SynthEd.</h1>
+          <h3>
+            Music to <span ref={textRef}></span>
+          </h3>
+
+          <div>
+            <Link to="/account" className="btn">
+              Sign Up
+            </Link>
+            <Link to="/account" className="btn btn-light">
+              Login
+            </Link>
+          </div>
         </div>
-        </div>
+      </div>
+    </div>
+  );
+};
 
-
-</div>
-  )
-}
-
-export default Hero
+export default Hero;
