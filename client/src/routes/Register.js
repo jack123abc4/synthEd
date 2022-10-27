4014943dfcab56a365b5cb15140b6e79c6c071a1
Lauren <lburke522@gmail.com>
2022-10-27 13:09:44 -0400

    fixed typos and added image

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../components/nav/Nav";
-import Register from '../components/account/git';
+import Register from '../components/account/Signup';
 
 
 const Signup = () => {
