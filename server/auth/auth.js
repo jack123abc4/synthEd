3f757f2ef23500277f64f83d7cf15d05fec27776
Lauren <lburke522@gmail.com>
2022-10-31 19:08:28 -0400

    some extra auth middleware

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -9,7 +9,6 @@ router.get("/login/success", (req, res) => {
       success: true,
       message: "successfull",
       user: req.user,
-      //   cookies: req.cookies
     });
   }
 });
@@ -25,35 +24,5 @@ router.get("/logout", (req, res) => {
   req.logout();
   res.redirect(CLIENT_URL);
 });
-
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed",
-  })
-);
-
-router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
-
-router.get(
-  "/github/callback",
-  passport.authenticate("github", {
-    successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed",
-  })
-);
-
-router.get("/twitter", passport.authenticate("twitter", { scope: ["profile"] }));
-
-router.get(
-  "/twitter/callback",
-  passport.authenticate("twitter", {
-    successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed",
-  })
-);
 
 module.exports = router
